c71fce2fc449e8a881a227c45294a028d2e9a5a9
Iuma <iumatoroyagari@gmail.com>
2022-11-17 17:43:08 -0500

    x

---
+++ Assets/Trigonometry/scripts/LookAt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+  
+    void Update()
+    {
+        Vector3 mousePosition = GetWorldMousePosition();
+        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x)- Mathf.PI/2;
+        RotateZ(angle);
+    }
+    private void RotateZ(float radianes)
+    {
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, radianes * Mathf.Rad2Deg);
+    }
+    private Vector4 GetWorldMousePosition()
+    {
+        Camera camera = Camera.main;
+        Vector3 screenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5);
+        Vector4 worldPos = camera.ScreenToWorldPoint(screenPos);
+        return worldPos;
+    }
+
+    
+}
---
+++ Assets/Trigonometry/scripts/Polar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Polar : MonoBehaviour
+{
+    [SerializeField] float radius;
+    [SerializeField] float angleDeg;
+
+    [Header("Speed and acceleration")]
+    [SerializeField] float angularSpeed;
+    [SerializeField] float angularAccel;
+    [SerializeField] float radialSpeed;
+    [SerializeField] float radialAccel;
+
+    [Header("World")]
+    [SerializeField] Transform bolita;
+    private void Start()
+    {
+        Assert.IsNotNull(bolita, "Bolita reference is required");
+    }
+
+    private void Update()
+    {
+      
+        angleDeg += angularSpeed * Time.deltaTime;
+        radius += radialSpeed * Time.deltaTime;
+
+        bolita.position = PolarToCartesian(radius, angleDeg);
+        Debug.DrawLine(Vector3.zero, PolarToCartesian(radius, angleDeg));
+    }
+
+    private Vector3 PolarToCartesian(float radius, float angle)
+    {
+        float x = radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        float y = radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        return new Vector3(x, y, 0);
+    }
+}
+
